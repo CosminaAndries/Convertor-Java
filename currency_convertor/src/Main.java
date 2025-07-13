0dc466ae28d4833dc4ff99e281b8952f5382010c
CosminaAndries <162496999+CosminaAndries@users.noreply.github.com>
2025-07-13 22:48:44 +0300

    Update Main.java

--- currency_convertor/src/Main.java
+++ currency_convertor/src/Main.java
@@ -1,147 +1,132 @@
+package org.example;
+import netscape.javascript.JSObject;
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.math.BigDecimal;
 
 public class Main {
-    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+    public static void main(String[] args) {
         JFrame Fereastra = new JFrame();
 
-        Fereastra.setSize(350, 200);
-        ;
-        Fereastra.setTitle("Fereastra principala");
+        Fereastra.setSize(400, 200);
+        Fereastra.setTitle("Convertor monetar");
         Fereastra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //JLabel mesaj = new JLabel("Bine ai venit în aplicația mea!");
-        // Fereastra.add(mesaj, BorderLayout.NORTH);
+        Fereastra.setLocationRelativeTo(null);
         JPanel panou = new JPanel();
+
+
+
         panou.setLayout(new FlowLayout());
-        //panel.setLayout(); // Setează layout-ul pentru panou
 
-        // Adaugă eticheta pentru suma de bani
-        JLabel suma = new JLabel("Introdu suma de bani:", SwingConstants.LEFT);
-        panou.add(suma); // Adaugă eticheta în panou
 
-        // Creează câmpul de text
-        JTextField camp_suma = new JTextField(20); // Lățimea câmpului de text
-        panou.add(camp_suma); // Adaugă câmpul de text în panou
+        JLabel suma = new JLabel("Introdu suma de bani:");
+        panou.add(suma);
 
-        // Adaugă panoul în fereastră
+        JTextField camp_suma = new JTextField(20);
+        panou.add(camp_suma);
         Fereastra.add(panou, BorderLayout.NORTH);
 
         JPanel panou_butoane = new JPanel();
-        //  panou.setLayout(new BoxLayout( panou_butoane, BoxLayout.Y_AXIS));
-        panou_butoane.setLayout(new FlowLayout());
+       panou_butoane.setLayout(new FlowLayout());
         JLabel val_curenta = new JLabel("Selecteaza moneda curenta:");
         panou_butoane.add(val_curenta);
-        String[] moneda_curenta = {"RON", "EUR", "USD", "GBP"};
+
+        String[] moneda_curenta = {"AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND",
+                "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF",
+                "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ",
+                "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES",
+                "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD",
+                "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB",
+                "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP",
+                "SLE", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH",
+                "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL"};
+
         JComboBox<String> ListaMonede_curente = new JComboBox<>(moneda_curenta);
         panou_butoane.add(ListaMonede_curente);
 
         JLabel val_selectata = new JLabel("Selecteaza moneda de schimb:");
-        panou_butoane.add(val_selectata);
+       panou_butoane.add(val_selectata);
         JButton ok = new JButton("OK");
         JPanel panou2 = new JPanel();
         panou2.setLayout(new FlowLayout());
         panou2.add(ok);
 
 
-        //  JButton ron = new JButton("RON");
-        // ron.setSize();
-        // panou_butoane.add(ron);
-        // JButton dolari = new JButton("DOLARS");
-        //panou_butoane.add(dolari);
-
-
-        String[] moneda_selectata = {"RON", "EUR", "USD", "GBP"};
+        String[] moneda_selectata = {"AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND",
+                "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF",
+                "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ",
+                "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES",
+                "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD",
+                "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB",
+                "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP",
+                "SLE", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH",
+                "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL"};
         JComboBox<String> ListaMonede_selectate_ = new JComboBox<>(moneda_selectata);
         panou_butoane.add(ListaMonede_selectate_);
-        Fereastra.add(panou_butoane, BorderLayout.CENTER);
+       Fereastra.add(panou_butoane, BorderLayout.CENTER);
         Fereastra.add(panou2, BorderLayout.SOUTH);
+       panou2.setBackground(Color.BLUE);
         Fereastra.setVisible(true);
 
         ok.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
+
                     double suma_primita = Double.parseDouble(camp_suma.getText());
+                    if (suma_primita < 0) {
+                        JOptionPane.showMessageDialog(null, "Introdu o sumă valida!");
+                        return;
+                    }
                     String dinMoneda = (String) (ListaMonede_curente.getSelectedItem());
                     String inMoneda = (String) (ListaMonede_selectate_.getSelectedItem());
+                    if(dinMoneda==inMoneda){
+                        JOptionPane.showMessageDialog(null, "Eroare! Monedele nu pot fi aceleasi!");
+                        return;
+                    }
                     double rezultat = converteste(suma_primita, dinMoneda, inMoneda);
 
 
-                    // Deschide o nouă fereastră pentru a afișa rezultatul
-                    JFrame fereastraRezultat = new JFrame("Rezultat");
-                    fereastraRezultat.setSize(300, 200);
-                    fereastraRezultat.setLocationRelativeTo(null);// Centrează fereastra
-                    JLabel labelRezultat = new JLabel("Rezultatul conversiei: " + rezultat);
+                  JFrame fereastraRezultat = new JFrame("Rezultat");
+                    fereastraRezultat.setSize(250, 100);
+                    fereastraRezultat.setLocationRelativeTo(null);
+                    JLabel labelRezultat = new JLabel("Rezultatul conversiei: " + rezultat+inMoneda.toLowerCase());
+
                     fereastraRezultat.add(labelRezultat);
                     fereastraRezultat.setVisible(true);
                 } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(null, "Introdu o sumă validă!");
+                   JOptionPane.showMessageDialog(null, "Introdu o suma valida!");
+                } catch (IOException ex)
+                    {throw new RuntimeException(ex);
                 }
             }
         });
 
     }
 
-    private static double converteste(double suma, String dinMoneda, String inMoneda) {
-        double rataConversie = obtineRataConversie(dinMoneda, inMoneda);
-        return suma * rataConversie;
+    private static double converteste(double suma, String dinMoneda, String inMoneda) throws IOException {
+        String url_adresa = "https://api.exchangerate.host/convert?access_key=8ab400f997ec752d24eed50a31cf4bb7&from=" + dinMoneda.toUpperCase() + "&to=" + inMoneda.toUpperCase() + "&amount=" + suma;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url_adresa)
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String stringResponse = response.body().string();
+        JSONObject jsObject = new JSONObject(stringResponse);
+        BigDecimal rate = jsObject.getJSONObject("info").getBigDecimal("quote");
+        double rata = rate.doubleValue();
+        return suma * rata;
     }
 
-    private static double obtineRataConversie(String dinMoneda, String inMoneda) {
-        if (dinMoneda.equals(inMoneda)) {
-            return 1;
-        }
-        switch (dinMoneda) {
-            case "RON":
-                switch (inMoneda) {
-                    case "EUR":
-                        return 0.2;
-                    case "USD":
-                        return 0.23;
-                    case "GBP":
-                        return 0.17;
-                }
-                break;
-            case "EUR":
-                switch (inMoneda) {
-                    case "RON":
-                        return 5;
-                    case "USD":
-                        return 1.1;
-                    case "GBP":
-                        return 0.85;
-                }
-                break;
-            case "USD":
-                switch (inMoneda) {
-                    case "RON":
-                        return 4.3;
-                    case "EUR":
-                        return 0.9;
-                    case "GBP":
-                        return 0.78;
-                }
-                break;
-            case "GBP":
-                switch (inMoneda) {
-                    case "RON":
-                        return 5.7;
-                    case "EUR":
-                        return 1.18;
-                    case "USD":
-                        return 1.28;
-                }
-                break;
-            default:
-                System.out.println("Moneda nu este suportată");
-                return 0;
-        }
-        return 0;
-    }
 }
-
-
 
